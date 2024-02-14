43ab1e416d3eb38e45b296200ba5e2165aaa4f5a
lazaralex98 <alex2012ro@gmail.com>
2024-02-14 21:26:45 +0200

    add json accepting returns

--- handler/post_handler.go
+++ handler/post_handler.go
@@ -23,39 +23,77 @@ func (h *Posts) RegisterRoutes(app *fiber.App) {
 
 // GET /posts - index - List all posts
 func (h *Posts) index(c *fiber.Ctx) error {
+	var isJSON bool
+	if c.Get("Accept") == "application/json" {
+		isJSON = true
+	}
+
 	var rows []model.Post
 	tx := model.DB.Find(&rows)
 	if tx.Error != nil {
+		if isJSON {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to retrieve posts"})
+		}
 		return RenderTempl(c, posts.Error("No posts found"))
 	}
 
+	if isJSON {
+		return c.JSON(rows)
+	}
 	return RenderTempl(c, posts.Index(rows))
 }
 
 // GET /posts/:id - show - Show a single post
 func (h *Posts) show(c *fiber.Ctx) error {
+	var isJSON bool
+	if c.Get("Accept") == "application/json" {
+		isJSON = true
+	}
+
 	idStr := c.Params("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
+		if isJSON {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid ID format"})
+		}
 		return RenderTempl(c, posts.Error("Invalid ID format"))
 	}
 
 	var res model.Post
 	tx := model.DB.First(&res, id)
 	if tx.Error != nil {
+		if isJSON {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "Post not found"})
+		}
 		return RenderTempl(c, posts.Error("Post not found"))
 	}
 
+	if isJSON {
+		return c.JSON(res)
+	}
 	return RenderTempl(c, posts.Show(res))
 }
 
 // GET /posts/new - new - Show the form to create a new post
 func (h *Posts) new(c *fiber.Ctx) error {
+	var isJSON bool
+	if c.Get("Accept") == "application/json" {
+		isJSON = true
+	}
+
+	if isJSON {
+		return c.Status(http.StatusNotAcceptable).JSON(fiber.Map{"error": "JSON not supported"})
+	}
 	return RenderTempl(c, posts.New())
 }
 
 // POST /posts - create - Create a new post
 func (h *Posts) create(c *fiber.Ctx) error {
+	var isJSON bool
+	if c.Get("Accept") == "application/json" {
+		isJSON = true
+	}
+
 	title := c.FormValue("title")
 	content := c.FormValue("content")
 	author := c.FormValue("author")
@@ -68,40 +106,71 @@ func (h *Posts) create(c *fiber.Ctx) error {
 
 	tx := model.DB.Create(&post)
 	if tx.Error != nil {
+		if isJSON {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to create post"})
+		}
 		return RenderTempl(c, posts.Error("Failed to create post"))
 	}
 
+	if isJSON {
+		return c.Status(http.StatusCreated).JSON(post)
+	}
 	return c.Redirect("/posts/"+strconv.Itoa(int(post.ID)), http.StatusFound)
 }
 
 // GET /posts/:id/edit - edit - Show the form to edit a post
 func (h *Posts) edit(c *fiber.Ctx) error {
+	var isJSON bool
+	if c.Get("Accept") == "application/json" {
+		isJSON = true
+	}
+
 	idStr := c.Params("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
+		if isJSON {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid ID format"})
+		}
 		return RenderTempl(c, posts.Error("Invalid ID format"))
 	}
 
 	var res model.Post
 	tx := model.DB.First(&res, id)
 	if tx.Error != nil {
+		if isJSON {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "Post not found"})
+		}
 		return RenderTempl(c, posts.Error("Post not found"))
 	}
 
+	if isJSON {
+		return c.JSON(res)
+	}
 	return RenderTempl(c, posts.Edit(res))
 }
 
 // PUT /posts/:id - update - Update a post
 func (h *Posts) update(c *fiber.Ctx) error {
+	var isJSON bool
+	if c.Get("Accept") == "application/json" {
+		isJSON = true
+	}
+
 	idStr := c.Params("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
+		if isJSON {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid ID format"})
+		}
 		return RenderTempl(c, posts.Error("Invalid ID format"))
 	}
 
 	var res model.Post
 	tx := model.DB.First(&res, id)
 	if tx.Error != nil {
+		if isJSON {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "Post not found"})
+		}
 		return RenderTempl(c, posts.Error("Post not found"))
 	}
 
@@ -113,31 +182,54 @@ func (h *Posts) update(c *fiber.Ctx) error {
 	res.Author = author
 	tx = model.DB.Save(&res)
 	if tx.Error != nil {
+		if isJSON {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to update post"})
+		}
 		return RenderTempl(c, posts.Error("Failed to update post"))
 	}
 
+	if isJSON {
+		return c.JSON(res)
+	}
 	return c.Redirect("/posts/"+strconv.Itoa(int(res.ID)), http.StatusFound)
 }
 
 // DELETE /posts/:id - delete - Delete a post
 func (h *Posts) delete(c *fiber.Ctx) error {
+	var isJSON bool
+	if c.Get("Accept") == "application/json" {
+		isJSON = true
+	}
+
 	idStr := c.Params("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
+		if isJSON {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid ID format"})
+		}
 		return RenderTempl(c, posts.Error("Invalid ID format"))
 	}
 
 	var res model.Post
 	tx := model.DB.First(&res, id)
 	if tx.Error != nil {
+		if isJSON {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "Post not found"})
+		}
 		return RenderTempl(c, posts.Error("Post not found"))
 	}
 
 	tx = model.DB.Delete(&res)
 	if tx.Error != nil {
+		if isJSON {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to delete post"})
+		}
 		return RenderTempl(c, posts.Error("Failed to delete post"))
 	}
 
+	if isJSON {
+		return c.Status(fiber.StatusNoContent).JSON(fiber.Map{"message": "Post deleted"})
+	}
 	return c.Redirect("/posts", http.StatusFound)
 }
 
