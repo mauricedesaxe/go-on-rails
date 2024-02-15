f5f10a3e4a4c671c854d56d6dc18892e77599eb7
lazaralex98 <alex2012ro@gmail.com>
2024-02-15 14:12:32 +0200

    move all utils in one file

--- handler/utils.go
+++ handler/utils.go
@@ -1,8 +1,26 @@
 package handler
 
 import (
+	"github.com/a-h/templ"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
 )
+
+// RouteRegistrar is an interface that defines the method that a route registrar must implement
+type RouteRegistrar interface {
+	RegisterRoutes(app *fiber.App)
+}
+
+// CompleteResourceController is an interface that defines the methods that a handler must implement
+type CompleteResourceController interface {
+	index(c *fiber.Ctx) error
+	show(c *fiber.Ctx) error
+	new(c *fiber.Ctx) error
+	create(c *fiber.Ctx) error
+	edit(c *fiber.Ctx) error
+	update(c *fiber.Ctx) error
+	delete(c *fiber.Ctx) error
+}
 
 // ResponseHandler defines a function that handles the response in a specific format.
 type ResponseHandler func(*fiber.Ctx) error
@@ -14,4 +32,12 @@ func HandleResponse(c *fiber.Ctx, handleJSON ResponseHandler, handleTempl Respon
 		return handleJSON(c)
 	}
 	return handleTempl(c)
+}
+
+func RenderTempl(c *fiber.Ctx, component templ.Component, options ...func(*templ.ComponentHandler)) error {
+	componentHandler := templ.Handler(component)
+	for _, o := range options {
+		o(componentHandler)
+	}
+	return adaptor.HTTPHandler(componentHandler)(c)
 }
