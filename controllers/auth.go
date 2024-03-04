5897fe5519cf694261d3492852bd70080c63fa33
lazaralex98 <alex2012ro@gmail.com>
2024-03-04 19:34:45 +0200

    add POST login route

--- controllers/auth.go
+++ controllers/auth.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/session"
 	models "github.com/mauricedesaxe/go-on-rails/models"
 	"github.com/mauricedesaxe/go-on-rails/views/auth"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthController struct {
@@ -15,6 +16,7 @@ type AuthController struct {
 func (a *AuthController) RegisterRoutes(app *fiber.App) {
 	app.Get("/profile", a.profile)
 	app.Get("/login", a.login)
+	app.Post("/login", a.doLogin)
 }
 
 // GET /profile - profile - Show the profile of the logged in user
@@ -45,4 +47,42 @@ func (a *AuthController) profile(c *fiber.Ctx) error {
 // GET /login - login - Show the login form
 func (a *AuthController) login(c *fiber.Ctx) error {
 	return RenderTempl(c, auth.Login())
+}
+
+// POST /login - doLogin - Process the login form
+func (a *AuthController) doLogin(c *fiber.Ctx) error {
+	// get session
+	sess, err := a.SessionStore.Get(c)
+	if err != nil {
+		return RenderTempl(c, auth.Error("Session error"))
+	}
+
+	// check if user is already logged in
+	userID := sess.Get("user_id")
+	if userID != nil {
+		return c.Redirect("/profile")
+	}
+
+	// get user from database
+	user := models.User{Username: c.FormValue("username")}
+	err = user.ReadByUsername()
+	if err != nil {
+		return RenderTempl(c, auth.Error("User not found"))
+	}
+
+	// check password
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(c.FormValue("password")))
+	if err != nil {
+		return RenderTempl(c, auth.Error("Invalid password"))
+	}
+
+	// set user id in session
+	sess.Set("user_id", user.ID)
+	err = sess.Save()
+	if err != nil {
+		return RenderTempl(c, auth.Error("Session error"))
+	}
+
+	// redirect to profile
+	return c.Redirect("/profile")
 }
